388cd7c124ab8f7870d0feb7af4dc023015fc69e
Deolyn East <deolyneast@gmail.com>
2026-04-15 07:11:30 +0000

    feat: align navbar routes with App.tsx and fix L.png import

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,8 +1,5 @@
 declare module "*.css";
-declare module "*.png" {
-  const value: string;
-  export default value;
-}
+declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.svg";
